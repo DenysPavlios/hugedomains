e5e615071cee2bcf22211b1a4390a14b384587cd
itnam <mcden@MacBook-Pro-itnam.local>
2024-02-16 18:49:53 +0200

    firefox

--- tests/test_home_search.py
+++ tests/test_home_search.py
@@ -5,9 +5,9 @@ from playwright.sync_api import sync_playwright, Page
 
 
 
-def test_search_query():
+def test_search_query(page):
     search = "DroneTe"
-    search_home = HomePage()
+    search_home = HomePage(page)
     search_home.fill_input__btn_click_for_search(search)
     time.sleep(5)
     search_home.wait_locator()
