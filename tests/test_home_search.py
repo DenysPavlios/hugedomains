b140b7796ca4f319e7e2ad35d68e114a8038ea9c
itnam <mcden@MacBook-Pro-itnam.local>
2024-02-18 16:33:11 +0200

    add pytest.fixture for test_search

--- tests/test_home_search.py
+++ tests/test_home_search.py
@@ -3,9 +3,16 @@ from pages.home_page import HomePage
 import pytest
 from playwright.sync_api import sync_playwright, Page
 
+@pytest.fixture
+def page():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        yield page
+        page.close()
+        browser.close()
 
-
-def test_search_query(page:Page):
+def test_search_query(page):
     search = "DroneTe"
     search_home = HomePage(page)
     search_home.fill_input__btn_click_for_search(search)
